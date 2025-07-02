9b582eee97f8d33f83cc122c71e8a44b39985f41
rusanovaleksandr <a.i.rusanov@mail.ru>
2025-07-02 22:04:57 +0300

    Add constants to remove duplicate strings

--- src/test/java/com/example/wikipedia/tests/FindArticlesTest.java
+++ src/test/java/com/example/wikipedia/tests/FindArticlesTest.java
@@ -1,53 +1,49 @@
 package com.example.wikipedia.tests;
 
-import org.junit.jupiter.api.Test;
-
-import com.example.wikipedia.services.TestResults;
-
 import java.util.HashMap;
 import java.util.Map;
 
-import com.example.wikipedia.pages.*;
+import org.junit.jupiter.api.Test;
+
+import com.example.wikipedia.pages.ArticlePage;
+import com.example.wikipedia.services.TestResults;
 
 
 public class FindArticlesTest extends BaseTest {
     private static final int MAX_ATTEMPTS = 10;
+    private static final String TEST_HAS_IMAGE = "test4_has_image";
+    private static final String TEST_SHORT_URL = "test5_short_url";
+    private static final String TEST_HAS_TITLE = "test6_has_title";
+    private static final String TEST_HAS_CITE = "test7_has_cite";
+    private static final String TEST_HAS_REFERENCES = "test8_has_references";
+    private static final String TEST_HAS_NOTES = "test9_has_notes";
+    private static final String TEST_HAS_ENGLISH = "test10_has_english";
+    private static final String TEST_HAS_PDF = "test11_has_pdf";
     
     @Test
     void findArticlesWithConditions() {
-        
         Map<String, Boolean> conditions = new HashMap<>() {{
-            put("test4_has_image", false);
-            put("test5_short_url", false);
-            put("test6_has_title", false);
-            put("test7_has_cite", false);
-            put("test8_has_references", false);
-            put("test9_has_notes", false);
-            put("test10_has_english", false);
-            put("test11_has_pdf", false);
+            put(TEST_HAS_IMAGE, false);
+            put(TEST_SHORT_URL, false);
+            put(TEST_HAS_TITLE, false);
+            put(TEST_HAS_CITE, false);
+            put(TEST_HAS_REFERENCES, false);
+            put(TEST_HAS_NOTES, false);
+            put(TEST_HAS_ENGLISH, false);
+            put(TEST_HAS_PDF, false);
         }};
         
         for (int i = 0; i < MAX_ATTEMPTS; i++) {
             if (!conditions.containsValue(false)) break;
-
             ArticlePage article = mainPage.openRandomArticle();
-            
-            checkAndUpdateCondition(conditions, article, "test4_has_image", article.hasImages());
-
-            checkAndUpdateCondition(conditions, article, "test5_short_url", article.hasShortUrlOption());
-
-            checkAndUpdateCondition(conditions, article, "test6_has_title",true);
-
-            checkAndUpdateCondition(conditions, article, "test7_has_cite", article.hasCiteOption());
-
-            checkAndUpdateCondition(conditions, article, "test8_has_references", article.hasReferencesSection());
-
-            checkAndUpdateCondition(conditions, article, "test9_has_notes", article.hasNotesSection());
-
-            checkAndUpdateCondition(conditions, article, "test10_has_english", article.hasEnglishVersion());
-
-            checkAndUpdateCondition(conditions, article, "test11_has_pdf", article.hasPdfDownload());      
-
+            checkAndUpdateCondition(conditions, article, TEST_HAS_IMAGE, article.hasImages());
+            checkAndUpdateCondition(conditions, article, TEST_SHORT_URL, article.hasShortUrlOption());
+            checkAndUpdateCondition(conditions, article, TEST_HAS_TITLE,true);
+            checkAndUpdateCondition(conditions, article, TEST_HAS_CITE, article.hasCiteOption());
+            checkAndUpdateCondition(conditions, article, TEST_HAS_REFERENCES, article.hasReferencesSection());
+            checkAndUpdateCondition(conditions, article, TEST_HAS_NOTES, article.hasNotesSection());
+            checkAndUpdateCondition(conditions, article, TEST_HAS_ENGLISH, article.hasEnglishVersion());
+            checkAndUpdateCondition(conditions, article, TEST_HAS_PDF, article.hasPdfDownload());      
         }
         
         conditions.forEach((testName, found) -> {
