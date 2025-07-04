e0b6434eb95bf2ca5e625cd7567a748a1df661e2
rusanovaleksandr <a.i.rusanov@mail.ru>
2025-07-04 18:04:26 +0300

    minor fix GalleryCloseButton

--- src/test/java/com/example/wikipedia/elements/GalleryCloseButton.java
+++ src/test/java/com/example/wikipedia/elements/GalleryCloseButton.java
@@ -7,7 +7,6 @@ import static java.time.Duration.ofSeconds;
 public class GalleryCloseButton extends BaseElement {
     private static final String TITLE_XPATH = "//button[contains(@title, '%s')]";
     private static final int TIMEOUT = 3; 
-    private static final String DEFAULT_CSS_SELECTOR = "button.mw-mmv-close";
     
     public GalleryCloseButton(String selector, boolean xpath) {
         super(selector, xpath);
