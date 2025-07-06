2447674acb78e0b9e293b9780950ad0c01d05fac
AleksandrSpiridonov <spiridon-2005@mail.ru>
2025-07-06 11:25:05 +0300

    docs added

--- src/test/java/com/example/wikipedia/elements/DownloadButton.java
+++ src/test/java/com/example/wikipedia/elements/DownloadButton.java
@@ -2,17 +2,39 @@ package com.example.wikipedia.elements;
 
 import java.io.File;
 
+/**
+ * Представляет кнопку скачивания на веб-странице.
+ * Наследует функциональность {@link BaseElement} и предоставляет методы
+ * для взаимодействия с элементами скачивания, включая загрузку PDF-файлов.
+ */
 public class DownloadButton extends BaseElement {
     private static final String TEXT_XPATH = "//button[.//span[text()='%s']]";
 
+    /**
+     * Конструктор для создания кнопки скачивания.
+     *
+     * @param selector селектор элемента
+     * @param xpath флаг, указывающий тип селектора (true - XPath, false - CSS)
+     */
     public DownloadButton(String selector, boolean xpath) {
         super(selector, xpath);
     }
 
+    /**
+     * Выполняет скачивание PDF-файла.
+     *
+     * @return скачанный файл в формате PDF
+     */
     public File downloadPdf() {
         return element.download();
     }
 
+    /**
+     * Фабричный метод для создания кнопки по тексту на ней.
+     *
+     * @param id текст кнопки
+     * @return экземпляр {@link DownloadButton}
+     */
     public static DownloadButton byText(String id) {
         return new DownloadButton(String.format(TEXT_XPATH, id), true);
     }
--- src/test/java/com/example/wikipedia/elements/FootnoteElement.java
+++ src/test/java/com/example/wikipedia/elements/FootnoteElement.java
@@ -1,13 +1,30 @@
 package com.example.wikipedia.elements;
 
+/**
+ * Представляет элемент сноски в статье.
+ * Наследует функциональность {@link ClickableElement} и предоставляет методы
+ * для взаимодействия со сносками через их номер.
+ */
 public class FootnoteElement extends ClickableElement {
     private static final String FOOTNOTE_XPATH =
             "//sup[@class='reference']/a[contains(@href, 'cite_note') and contains(text(), '%s')]";
 
+    /**
+     * Конструктор для создания элемента сноски.
+     *
+     * @param selector селектор элемента
+     * @param xpath флаг, указывающий тип селектора (true - XPath, false - CSS)
+     */
     public FootnoteElement(String selector, boolean xpath) {
         super(selector, xpath);
     }
 
+    /**
+     * Фабричный метод для создания элемента по номеру сноски.
+     *
+     * @param number номер сноски
+     * @return экземпляр {@link FootnoteElement}
+     */
     public static FootnoteElement byFootnoteNumber(String number) {
         String xpath = String.format(FOOTNOTE_XPATH, number);
         return new FootnoteElement(xpath, true);
